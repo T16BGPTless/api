89075dfd45f5308657c05e17afd7fda52c99cdd0
bevyn11 <bevyn102@gmail.com>
2026-03-06 20:49:23 +1100

    Delete Placeholder unneeded code

--- frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders invoice generation heading", () => {
-  render(<App />);
-  const heading = screen.getByText(/Invoice Generation SaaS/i);
-  expect(heading).toBeInTheDocument();
-});
